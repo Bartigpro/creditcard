4f29f96ad5f0ebbac24c57aa7537bfb339973583
Bartosz Gańko <125035686+Bartigpro@users.noreply.github.com>
2023-09-14 10:31:19 +0200

    adadad
    asadasd

--- client/app.js
+++ client/app.js
@@ -47,16 +47,220 @@ function img(id){
    
     console.log(id)
     if(id==1){
-        document.getElementById("karta").style.backgroundImage ="url('R.jfif')"
+        document.getElementById("tilt").style.backgroundImage ="url('R.jfif')"
     }
     else if(id==2){
-        document.getElementById("karta").style.backgroundImage ="url('th.jfif')"
+        document.getElementById("tilt").style.backgroundImage ="url('th.jfif')"
     }
     
 
 else{
-    document.getElementById("karta").style.backgroundImage ="url('pobierz.jfif')"
+    document.getElementById("tilt").style.backgroundImage ="url('pobierz.jfif')"
     
 
 }
 }
+
+
+
+function zamiennumer(){
+    const numer = document.getElementById("nmb").value
+    document.getElementById("NO").innerHTML = numer
+    console.log(numer)
+    
+
+
+    
+ 
+    const h2Element = document.getElementById("NO");
+
+   
+    const tekst = h2Element.textContent;
+
+
+    let nowyTekst = '';
+    
+  
+    if(tekst.length<=13){
+    for (let i = 0; i < tekst.length; i++) {
+       
+        nowyTekst += tekst[i];
+        
+        if ((i + 1) % 3 === 0 && i !== tekst.length - 1) {
+            if(nowyTekst.length<=13){
+            nowyTekst += ' ';}
+        }
+        
+      
+    }
+    }
+    
+    
+    h2Element.textContent = nowyTekst;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Store the element in el */
+let el = document.getElementById('tilt')
+
+/* Get the height and width of the element */
+const height = el.clientHeight
+const width = el.clientWidth
+
+/*
+  * Add a listener for mousemove event
+  * Which will trigger function 'handleMove'
+  * On mousemove
+  */
+el.addEventListener('mousemove', handleMove)
+
+/* Define function a */
+function handleMove(e) {
+  /*
+    * Get position of mouse cursor
+    * With respect to the element
+    * On mouseover
+    */
+  /* Store the x position */
+  const xVal = e.layerX
+  /* Store the y position */
+  const yVal = e.layerY
+  
+  /*
+    * Calculate rotation valuee along the Y-axis
+    * Here the multiplier 20 is to
+    * Control the rotation
+    * You can change the value and see the results
+    */
+  const yRotation = 20 * ((xVal - width / 2) / width)
+  
+  /* Calculate the rotation along the X-axis */
+  const xRotation = -20 * ((yVal - height / 2) / height)
+  
+  /* Generate string for CSS transform property */
+  const string = 'perspective(500px) scale(1.1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+  
+  /* Apply the calculated transformation */
+  el.style.transform = string
+}
+
+/* Add listener for mouseout event, remove the rotation */
+el.addEventListener('mouseout', function() {
+  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mousedown event, to simulate click */
+el.addEventListener('mousedown', function() {
+  el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mouseup, simulate release of mouse click */
+el.addEventListener('mouseup', function() {
+  el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+})
